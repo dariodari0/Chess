3e2145e8190125475831ea568f6d088edfd2df61
wyrobekj <wyrobekj@interia.pl>
2017-07-08 10:44:50 +0200

    formatkaDoktora2
    1. dołożone funkcje, które były w template od Doktora
    2. napisałam funkcję valid(line)
    3. usunęłam dyrektywy preprocesora
    4. dostosowałam funkcje translate oraz displayBoard aby działały dla dowolnej wielkości tablicy
    5.  napisałam readMove

--- chess.cpp
+++ chess.cpp
@@ -1,224 +1,4 @@
-// important for VS
-#include "stdafx.h"
-
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <sstream>
-
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
-
-#ifndef BEGIN
-#define BEGIN    "      A   B   C   D   E   F   G   H      \n"
-#endif
-#ifndef FRINGE
-#define FRINGE   "    +---+---+---+---+---+---+---+---+   \n"
-#endif
-#ifndef EMPTY
-#define EMPTY " |   |   |   |   |   |   |   |   | "
-#endif
-#ifndef END
-#define END    "      A   B   C   D   E   F   G   H      \n"
-#endif
-
-const char startup[8][8] = { 'r', 'b', 'n', 'q', 'k', 'n', 'b', 'r', 'p', 'p','p','p','p','p','p', 'p', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'R', 'B', 'N', 'Q', 'K', 'N', 'B', 'R' };
-
-
-using namespace std;
-
-const int BOARD_SIZE = 8;
-
-char board[BOARD_SIZE][BOARD_SIZE];
-
-
-#ifdef _WIN32
-void gotoXY(short x, short y)
-{
-	COORD coord = { x, y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-#else
-void gotoXY(short x, short y)
-{
-	cout << "\033[" << y << ";" << x << "f" << flush;
-}
-#endif
-
-void clearLine(short y)
-{
-	gotoXY(0, y);
-	cout << setw(128) << " ";
-	gotoXY(0, y);
-}
-
-
-void clearScreen()
-{
-#ifdef _WIN32
-	system("cls");
-#else
-	system("clear");
-#endif
-}
-
-
-void clearLinesFrom(short y, short count)
-{
-	for (short i = 0; i < count; i++)
-		clearLine(y + i);
-}
-
-
-struct Move {
-	char fromCh;
-	int fromInt;
-	char toCh;
-	int toInt;
-};
-
-
-Move getMove() {
-
-	Move move;
-	string passd;
-	char *temp;
-	getline(cin, passd);
-
-
-	istringstream iss(passd);
-	iss >> move.fromCh;
-	iss >> move.fromInt;
-	iss >> move.toCh;
-	iss >> move.toInt;
-
-	move.fromCh = toupper(move.fromCh);
-	move.toCh = toupper(move.toCh);
-
-
-	return move;
-}
-
-//function validates the move whether it is within the chessboard
-//letter >= A i <= H and number >= 1 <= 8
-//and returns proper bool value
-//struct Move {
-//char fromCh;
-//int fromInt;
-//char toCh;
-//int toInt; }
-bool valid(Move m)
-{
-
-	if (m.fromCh < 'A' || m.fromCh >= 'A'+BOARD_SIZE) {
-		return false;
-	}
-	else if (m.toCh < 'A' || m.toCh >= 'A'+BOARD_SIZE) {
-		return false;
-	}
-	else if (m.fromInt < 1 || m.fromInt >= BOARD_SIZE) {
-		return false;
-	}
-	else if (m.toInt < 1 || m.toInt >= BOARD_SIZE) {
-		return false;
-	}
-	return true;
-}
-
-
-int translateLetter(char letter) {
-	int result = letter - 'A';
-	if (result >= 0 && result <BOARD_SIZE) {
-		return result;
-	}
-	else
-	{
-		throw "Nie ma takiej kolumny";
-	}
-
-}
-
-int translateInt(int number) {
-
-	if (number < 0 && number > BOARD_SIZE) {
-		throw "Nie ma takiego numeru wiersza na szachownicy!";
-	}
-	number = number - 1;
-	//mozna to zrobic sprytniej, ale tak jest czytelnie
-	//zamieniam numery bo tablica ma odwrotnie numeracje niz to co sie wyswietla na szachownicy
-
-	switch (number) {
-	case 0:
-		return 7;
-	case 1:
-		return 6;
-	case 2:
-		return 5;
-	case 3:
-		return 4;
-	case 4:
-		return 3;
-	case 5:
-		return 2;
-	case 6:
-		return 1;
-	case 7:
-		return 0;
-	}
-	//żeby kompilator nie darl sie, blad wychwyci walidacja
-	return 100;
-
-}
-
-
-void makeMove(Move m, char board[][BOARD_SIZE])
-{
-	int columnFrom = translateLetter(m.fromCh);
-	int columnTo = translateLetter(m.toCh);
-	int rowFrom = translateInt(m.fromInt);
-	int rowTo = translateInt(m.toInt);
-	char figure = ' ';
-	/*
-	if (board[rowFrom][columnFrom] == ' ') {
-		throw "Niepoprawny ruch, na polu startowym nie ma nic!";
-	}
-	if (board[rowTo][columnTo] != ' ') {
-		throw "Niepoprawny ruch, pole docelowe jest zajęte!";
-	}
-	*/
-	figure = board[rowFrom][columnFrom];
-	board[rowFrom][columnFrom] = ' ';
-	board[rowTo][columnTo] = figure;
-	figure = ' ';
-
-}
-
-
-//displays board - takes array board[8][8]
-// and reads its contents, and displays on the screen
-void displayBoard(char board[][BOARD_SIZE])
-{
-	cout << BEGIN;
-	cout << FRINGE;
-
-	for (int i = 0; i<8; i++) {
-		cout << " " << 8 - i << " ";
-		for (int j = 0; j<8; j++) {
-			cout << " " << "|" << " " << board[i][j];
-		}
-		cout << " " << "|" << " " << 8 - i << " ";
-		cout << "\n";
-		cout << FRINGE;
-
-	}
-	cout << END;
-	cout << "\n";
-}
-
-//initializes board according to the following schema
-//      A   B   C   D   E   F   G   H
+//     A   B   C   D   E   F   G   H
 //   +---+---+---+---+---+---+---+---+
 // 8 | r | b | n | q | k | n | b | r | 8
 //   +---+---+---+---+---+---+---+---+
@@ -236,73 +16,414 @@ void displayBoard(char board[][BOARD_SIZE])
 //   +---+---+---+---+---+---+---+---+
 // 1 | R | B | N | Q | K | N | B | R | 1
 //   +---+---+---+---+---+---+---+---+
-//      A   B   C   D   E   F   G   H
+//     A   B   C   D   E   F   G   H
+
+
+//LIBRARIES --------------------------------------------------------------------------------------
+// important for VS
+//#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+using namespace std;
+
+//CONSTANTS ----------------------------------------------------------------------------------------
+const int BOARD_SIZE = 8;
+
+
+//GLOBAL VARIABLES ---------------------------------------------------------------------------------
+
+char board[BOARD_SIZE][BOARD_SIZE] = {' '};
+const char startup[8][8] = { 'r', 'b', 'n', 'q', 'k', 'n', 'b', 'r', 'p', 'p','p','p','p','p','p', 'p', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'R', 'B', 'N', 'Q', 'K', 'N', 'B', 'R'};
+
+struct Move {
+    char fromCh;
+    int fromInt;
+    char toCh;
+    int toInt;
+};
+
+//GLOBAL FUNCTIONS ---------------------------------------------------------------------------------
+
+#ifdef _WIN32
+void gotoXY(short x, short y)
+{
+	COORD coord = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+#else
+void gotoXY(short x, short y)
+{
+    cout << "\033[" << y << ";" << x << "f" << flush;
+}
+#endif
+
+
+
+void clearLine(short y)
+{
+    gotoXY(0, y);
+    cout << setw(128) << " ";
+    gotoXY(0, y);
+}
+
+
+
+void clearScreen()
+{
+#ifdef _WIN32
+        system("cls");
+#else
+    system("clear");
+#endif
+}
+
+//function so far is never used
+/*
+void clearLinesFrom(short y, short count)
+{
+    for (short i = 0; i < count; i++)
+        clearLine(y + i);
+}
+*/
+
+
+//removes spaces from char*
+void removeSpaces(char* s)
+{
+    char* s2 = s;
+    do {
+        if (*s2 != ' ')
+            *s++ = *s2;
+    } while (*s2++);
+}
+
+
+
+// Ta funkcja sprawdza czy ruch jest poprawny pod kątem:
+// - czy na polu startowym jest moja figura
+// - czy pole docelowe jest wolne lub czy stoi na nim figura przeciwnika
+// - czy ruch dla danej figury jest dopuszczalny (pomijamy roszadę, ale nalezy uwzględnić, że pierwszy ruch każdego piona może być o dwie pozycje do przodu)
+bool valid(const Move& m, char board[][8], bool whites)
+{
+    return true;
+}
+
+
+
+
+//function validates the move whether it is within the chessboard
+//letter >= A i <= H and number >= 1 <= 8
+//and returns proper bool value
+//struct Move {
+//char fromCh;
+//int fromInt;
+//char toCh;
+//int toInt; }
+bool valid(Move m)
+{
+
+    if(m.fromCh < 'A' || m.fromCh >= 'A' + BOARD_SIZE){
+        return false;
+    }
+    else if(m.toCh < 'A' || m.toCh >= 'A' + BOARD_SIZE ){
+        return false;
+    }
+    else if(m.fromInt < 1 || m.fromInt > BOARD_SIZE){
+        return false;
+    }
+    else if(m.toInt < 1 || m.toInt > BOARD_SIZE){
+        return false;
+    }
+    return true;
+}
+
+
+
+// Sprawdza czy linia jest w postaci litera liczba litera liczba oraz zamienia litery małe na wielkie, ewentualnie usuwa także spacje np. c    1    D 3 zmiania na C1D3
+bool valid(char line[])
+{
+    int length = 0;
+    int i=0, i1 = 0;
+    removeSpaces(line);
+
+    //length of array with characters
+    while(line[length]!='\0'){
+        length++;
+    }
+
+    //replace small letters into capital letters, no verification yet as to whether it is within BOARD_SIZE
+    for(int j=0; j<length; j++){
+        if(line[j]>='a' && line[j]<='z'){
+            line[j] = toupper(line[j]);
+        }
+    }
+
+    while(line[i]>='A' && line[i]<='Z'){
+        i++;
+    }
+
+    if(i==0){
+        cout << "Zly format wspolrzednych ruchu" << endl << flush;
+        return false;
+    }
+    else{
+        i1 = i;
+    }
+
+    while(line[i]>='0' && line[i]<='9'){
+        i++;
+    }
+
+    if(i==i1){
+        cout << "Zly format wspolrzednych ruchu" << endl << flush;
+        return false;
+    }
+    else
+    {
+        i1 = i;
+    }
+
+    while(line[i]>='A' && line[i]<='Z'){
+        i++;
+    }
+
+    if(i==i1){
+        cout << "Zly format wspolrzednych ruchu" << endl << flush;
+        return false;
+    }
+    else{
+        i1 = i;
+    }
+
+    while(line[i]>='0' && line[i]<='9'){
+        i++;
+    }
+
+    if(i==i1){
+        cout << "Zly format wspolrzednych ruchu" << endl << flush;
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+
+}
+
+
+
+//jawna translacja litery ze struktury Move do współrzędnych używanych na szachownicy
+int translateLetter(char letter) {
+    int result = letter - 'A';
+    if (result >= 0 && result < BOARD_SIZE) {
+        return result;
+    }
+    return -1;
+
+}
+
+
+
+//jawna transalcja cyfry ze struktury Move do współrzędnych używanych na szachownicy
+int translateInt(int number) {
+
+    if (number < 1 && number > BOARD_SIZE) {
+        cout <<  "Nie ma takiego numeru wiersza na szachownicy!" << endl << flush;
+        return -1;
+    }
+    //change numbering of rows - in MOVE struct there was numbering from 1 to BOARD_SIZE
+    //we change it initially to numbering from 0 to BOARD_SIZE - 1
+    //reverse numbers of rows to correctly address the array board
+    return BOARD_SIZE - number;
+}
+
+
+
+// zamienia ciag znaków w postaci litera liczba litera liczba na strukturę Move
+Move readMove(const char line[])
+{
+    char fromChars[20] = {' '};
+    char toChars[20] = {' '};
+    int fromInt = 0;
+    int toInt = 0;
+    int counter = 0;
+    int i = 0;
+
+
+    //read first symbol (letters)
+    while(line[i]>='A' && line[i]<'Z'){
+        if(line[i]==' '){
+            i++;
+        }
+        fromChars[counter] = line[i];
+        counter++;
+        line++;
+    }
+
+    //read second symbol (integer)
+    while(line[i]>='0' && line[i]<'9'){
+        if(line[i]==' '){
+            i++;
+        }
+        fromInt = fromInt*10 + (line[i]-'0');
+        i++;
+    }
+
+    counter = 0;
+    //read third symbol (letters)
+    while(line[i]>='A' && line[i]<'Z'){
+        if(line[i]==' '){
+            i++;
+        }
+        toChars[counter] = line[i];
+        counter++;
+        i++;
+    }
+
+    //read fourth symbol (integer)
+    while(line[i]>='0' && line[i]<'9'){
+        if(line[i]==' '){
+            i++;
+        }
+        toInt = toInt*10 + (line[i]-'0');
+        i++;
+    }
+
+    Move move;
+    strcpy(&move.fromCh, fromChars);
+    move.fromInt = fromInt;
+    strcpy(&move.toCh, toChars);
+    move.toInt = toInt;
+
+    return move;
+}
+
+Move getMove()
+{
+    Move m;
+    string line;
+    char lineStr[1024];
+    do {
+        getline(cin, line);
+        strcpy(lineStr, line.c_str());
+    } while (!valid(lineStr) || !valid((m = readMove(lineStr))));
+    return m;
+}
+
+void makeMove(Move m, char board[][BOARD_SIZE])
+{
+    int columnFrom = translateLetter(m.fromCh);
+    int columnTo = translateLetter(m.toCh);
+    int rowFrom = translateInt(m.fromInt);
+    int rowTo = translateInt(m.toInt);
+    char figure = board[rowFrom][columnFrom];
+    board[rowFrom][columnFrom] = ' ';
+    board[rowTo][columnTo] = figure;
+}
+
+
+//displays board - takes array board[BOARD_SIZE][BOARD_SIZE]
+// and reads its contents, and displays on the screen
+void displayBoard(char board[][BOARD_SIZE])
+{
+    //variable letter is used in a loop to display letters representing columns
+    //the loop automatically displays chars, simpler loops returned intergers instead of chars
+
+    char letter;
+    int i, j;
+    char k;
+
+    //row with letters that reprezenting headers of columns
+    cout << "   ";
+    for(letter = 'A'; letter < 'A'+ BOARD_SIZE; letter++)
+        cout << "   " <<  letter;
+    cout << "      \n";
+
+    //row with a delimited in a chessboard  "    +---+---+---+---+---+---+---+---+   \n";
+    cout << "    ";
+    for( i='A'; i<'A'+ BOARD_SIZE; i++){
+        cout << "+---";
+    }
+    cout << "+   \n";
+
+    //now we take the array that keeps actual positions of figures and display it on the chessboard
+
+    for( i=0; i < BOARD_SIZE; i++) {
+
+        cout << " " << BOARD_SIZE - i << " ";
+
+        for (j = 0; j < BOARD_SIZE; j++) {
+            cout << " " << "|" << " " << board[i][j];
+        }
+        cout << " " << "|" << " " << BOARD_SIZE - i << " ";
+        cout << "\n";
+
+
+        //now we delimit each row with figures with a delimiter "    +---+---+---+---+---+---+---+---+   \n";
+        cout << "    ";
+        for(k='A'; k<'A'+ BOARD_SIZE; k++){
+            cout << "+---";
+        }
+        cout << "+   \n";
+
+    }
+
+    //finally the bottom letters
+
+    //row with letters that reprezenting headers of columns
+    cout << "   ";
+    for(letter = 'A'; letter < 'A'+ BOARD_SIZE; letter++)
+        cout << "   " <<  letter;
+    cout << "      \n";
+
+}
+
+
 void initBoard(char board[][BOARD_SIZE])
 {
 	clearScreen();
-	//rząd 8
-	board[7][0] = 'R';
-	board[7][1] = 'B';
-	board[7][2] = 'N';
-	board[7][3] = 'Q';
-	board[7][4] = 'K';
-	board[7][5] = 'N';
-	board[7][6] = 'B';
-	board[7][7] = 'R';
-	//rząd 7
-	for (int i = 0; i<8; i++) {
-		board[6][i] = 'P';
-	}
-	//rzędy 6 - 3
-	for (int i = 2; i<6; i++) {
-		for (int j = 0; j<8; j++) {
-			board[i][j] = ' ';
-		}
-	}
 
-	//rząd 2
-	for (int i = 0; i<8; i++) {
-		board[1][i] = 'p';
-	}
-
-	//rząd 1
-	board[0][0] = 'r';
-	board[0][1] = 'b';
-	board[0][2] = 'n';
-	board[0][3] = 'q';
-	board[0][4] = 'k';
-	board[0][5] = 'n';
-	board[0][6] = 'b';
-	board[0][7] = 'r';
+    int i, j;
+    for (i = 0; i < BOARD_SIZE; i++) {
+        for (j = 0; j < BOARD_SIZE; j++) {
+            board[i][j] = startup[i][j]; //setup starting position
+        }
+    }
 }
 
 
 void doMove(char board[][BOARD_SIZE])
 {
-	cout << "Your move:" << endl << flush;
-	Move m = getMove();
+    cout << "Your move:" << endl << flush;
+    Move m = getMove();
 
-	while (!valid(m)) {
-		clearLine(20);
-		cout << "Error. Try again:" << endl << flush;
-		m = getMove();
-	}
-	makeMove(m, board);
-	
+    while (!valid(m)) {
+        clearLine(20);
+        cout << "Error. Try again:" << endl << flush;
+        m = getMove();
+    }
+    makeMove(m, board);
+
 }
-
 
 bool endOfGame(char board[][BOARD_SIZE])
 {
 	return false;
 }
 
-int main()
+int main() 
 {
 	initBoard(board);
+
 	do {
 		displayBoard(board);
 		doMove(board);
-		clearScreen();
 	} while (!endOfGame(board));
+
 }
